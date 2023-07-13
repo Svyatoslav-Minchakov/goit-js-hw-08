9fd15b05001758741cb9fd62615d7fb2719b87e9
Svyatoslav-Minchakov <76057514+Svyatoslav-Minchakov@users.noreply.github.com>
2023-07-13 10:55:30 +0000

    Deploying to gh-pages from @ Svyatoslav-Minchakov/goit-js-hw-08@91745b835d585abbc6ed328a7a695ab361d4b3c4 🚀

---
+++ 02-video.84b3a4e4.js
@@ -0,0 +1,2 @@
+console.log(15),console.log(15),console.log(15);
+//# sourceMappingURL=02-video.84b3a4e4.js.map
---
+++ 02-video.e1136b7c.js
@@ -0,0 +1,2 @@
+console.log(15),console.log(15),console.log(15);
+//# sourceMappingURL=02-video.e1136b7c.js.map
