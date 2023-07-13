91745b835d585abbc6ed328a7a695ab361d4b3c4
Святослав Минчаков <sminchakov@gmail.com>
2023-07-13 13:53:47 +0300

    first task added

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
-console.log(galleryItems);
+console.log(galleryItems)
+
+
+const galleryItemsArray = galleryItems.map(item => {
+  return `<li class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+     <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+  </a>
+</li>`
+}).join('');
+
+const galleryContainer = document.querySelector('.gallery');
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryItemsArray);
+
+galleryContainer.addEventListener('click', openImage);
+
+
+
+let lightbox;
+
+function openImage(event) {
+  event.preventDefault();
+  if(event.target.tagName !== 'IMG') return;
+  lightbox = new SimpleLightbox('.gallery a', {captionsData: 'Alt', captionDelay: '250'});
+  galleryContainer.removeEventListener('click', openImage);
+}
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,5 @@
 
+const a = 5 + 10;
+console.log(a)
+console.log(a)
+console.log(a)
