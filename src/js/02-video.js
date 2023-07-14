983850d7ad0d1a0104a0893125ba25c775d29de6
Святослав Минчаков <sminchakov@gmail.com>
2023-07-14 22:07:23 +0300

    hw was finished

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,5 +1,52 @@
+import Player from '@vimeo/player'; 
+import throttle from 'lodash.throttle';
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-const a = 5 + 10;
-console.log(a)
-console.log(a)
-console.log(a)
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+let isSeeking = false;
+
+player.on('play', function() {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function(title) {
+  console.log('title:', title);
+});
+
+player.ready().then(() => {
+  const storedTime = localStorage.getItem('videoplayer-current-time');
+
+  if (storedTime && storedTime < player.getDuration()) { 
+    player.setCurrentTime(storedTime).catch(error => {
+      console.error('Failed to set current time:', error); 
+    });
+  }
+
+  player.on('timeupdate', throttle(() => {
+    if (!isSeeking) { 
+      const currentTime = player.getCurrentTime().then(time => {
+        localStorage.setItem('videoplayer-current-time', time); 
+      }).catch(error => {
+        console.error('Failed to get current time:', error);
+      });
+    }
+  }, 1000));
+
+  player.on('seeking', () => {
+    isSeeking = true; 
+  });
+
+  player.on('seeked', () => {
+    isSeeking = false; 
+  });
+
+  window.addEventListener('beforeunload', () => {
+    const currentTime = player.getCurrentTime().then(time => {
+      localStorage.setItem('videoplayer-current-time', time); 
+    }).catch(error => {
+      console.error('Failed to get current time:', error);
+    });
+  });
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
+import "simplelightbox/dist/simple-lightbox.min.css";
 
+const pageForm = document.querySelector('.feedback-form');
+const pageEmail = document.querySelector('input');
+const pageMessage = document.querySelector('textarea');
+const pageButton = document.querySelector('button');
+
+getSaveMessage()
+
+console.log(pageButton)
+
+let formData;
+
+pageForm.addEventListener('input', throttle(enterText, 1000));
+pageForm.addEventListener('submit', sendText);
+
+function enterText() {
+    formData = {
+        email: pageEmail.value,
+        message: pageMessage.value
+    }
+    localStorage.setItem("feedback-form-state", JSON.stringify(formData))
+
+}
+
+function sendText(event) {
+    event.preventDefault();
+    event.target.reset();
+    localStorage.removeItem("feedback-form-state")
+    console.log(formData);
+}
+
+function getSaveMessage() {
+    const saveMessage = JSON.parse(localStorage.getItem("feedback-form-state"));
+    if (saveMessage) {
+        pageEmail.value = saveMessage.email,
+        pageMessage.value = saveMessage.message
+    }
+}
