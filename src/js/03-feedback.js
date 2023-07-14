78ae98bc94c15ab6e913a0134abaf8f182bdc189
Святослав Минчаков <sminchakov@gmail.com>
2023-07-14 22:58:20 +0300

    task2 was fixed

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,37 +4,38 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 const pageForm = document.querySelector('.feedback-form');
 const pageEmail = document.querySelector('input');
 const pageMessage = document.querySelector('textarea');
-const pageButton = document.querySelector('button');
+
+let formData = null;
 
 getSaveMessage()
-
-console.log(pageButton)
-
-let formData;
 
 pageForm.addEventListener('input', throttle(enterText, 1000));
 pageForm.addEventListener('submit', sendText);
 
 function enterText() {
     formData = {
-        email: pageEmail.value,
-        message: pageMessage.value
+        email: pageEmail.value.trim(),
+        message: pageMessage.value.trim()
     }
     localStorage.setItem("feedback-form-state", JSON.stringify(formData))
 
 }
 
 function sendText(event) {
+    if (pageEmail.value === '' && pageMessage.value === '') return;
     event.preventDefault();
     event.target.reset();
     localStorage.removeItem("feedback-form-state")
     console.log(formData);
+    formData = null;
 }
 
 function getSaveMessage() {
-    const saveMessage = JSON.parse(localStorage.getItem("feedback-form-state"));
-    if (saveMessage) {
-        pageEmail.value = saveMessage.email,
-        pageMessage.value = saveMessage.message
+    const savedMessage = JSON.parse(localStorage.getItem("feedback-form-state"));
+    if (savedMessage) {
+        pageEmail.value = savedMessage.email;
+        pageMessage.value = savedMessage.message;
+        formData = savedMessage;
     }
+    
 }
